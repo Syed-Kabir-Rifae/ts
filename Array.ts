58ecef956e129e8c272c5472b2a2fa8b51246061
Syed-Kabir-Rifae <68635020+Syed-Kabir-Rifae@users.noreply.github.com>
2022-03-23 11:15:55 +0530

    Create Array.ts

---
+++ Array.ts
@@ -0,0 +1,19 @@
+var arr =[2,4,455,54,5];
+var sum=1;
+for(let i=1;i<arr.length;i++)
+  {
+    sum=sum+arr[i]*i;  
+  console.log(sum);
+  }
+var arra_obj:string[] =new Array ("Syed","Ahamed","Kabir","rifae")
+for (let index =0; index < arra_obj.length; index++)
+  {
+    console.log(arra_obj);
+  }
+}
+var j:any; 
+var nums:number[] = [101,102,10,1] 
+
+for(j in nums) { 
+   console.log(nums[j]) 
+} 
